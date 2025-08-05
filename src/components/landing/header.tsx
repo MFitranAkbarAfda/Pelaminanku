332d0db8ae70e0a5328c65b5a9b18b69c2d2dc04
MFitranAkbarAfda <muhammadfitran52@gmail.com>
2025-08-05 14:36:25 +0700

    Update error merge

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> bc4ece16c0f25eda231fa9b327080cbcd570c534
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -52,15 +49,12 @@ export default function Header() {
               {link.label}
             </Link>
           ))}
-<<<<<<< HEAD
            <Link
               href="/dashboard"
               className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground"
             >
               Dashboard
             </Link>
-=======
->>>>>>> bc4ece16c0f25eda231fa9b327080cbcd570c534
         </nav>
         <div className="hidden items-center gap-2 md:flex">
           <Button variant="ghost">Masuk</Button>
@@ -93,7 +87,6 @@ export default function Header() {
               </Link>
             ))}
              <Link
-<<<<<<< HEAD
                 href="/dashboard"
                 className="text-lg font-medium text-muted-foreground transition-colors hover:text-foreground"
                 onClick={() => setIsMenuOpen(false)}
@@ -101,8 +94,6 @@ export default function Header() {
                 Dashboard
               </Link>
              <Link
-=======
->>>>>>> bc4ece16c0f25eda231fa9b327080cbcd570c534
                 href="#join"
                 className="text-lg font-medium text-muted-foreground transition-colors hover:text-foreground"
                 onClick={() => setIsMenuOpen(false)}
